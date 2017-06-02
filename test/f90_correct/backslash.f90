5089928cfc2ecf10827cb76398617c41512f00d1
Tim Keith <tkeith@nvidia.com>
2017-06-02 07:19:15 -0700

    Back out changelist 131610

--- test/f90_correct/backslash.f90
+++
@@ -1,7 +0,0 @@
-! RUN: %flang -c -Mbackslash %s
-! RUN: %flang -c -fno-backslash %s
-! RUN: not %flang -c -Mnobackslash %s 2>&1 | FileCheck %s
-! RUN: not %flang -c -fbackslash %s 2>&1 | FileCheck %s
-
-write (*,*) "\" ! CHECK: Unmatched quote
-end
